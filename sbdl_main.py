031d33752be5dceecf02fd152865776547dbc788
ProfessorXcommunity <irealarijit2@gmail.com>
2025-09-14 00:21:26 +0530

    starter updated

--- sbdl_main.py
+++ sbdl_main.py
@@ -1,6 +1,18 @@
-from pyspark.sql import SparkSession
+import sys
 
-spark = SparkSession.builder \
-    .appName("MDM to Kafka ETL") \
-    .getOrCreate()
+from lib import Utils
+from lib.logger import Log4j
 
+if __name__ == '__main__':
+
+    if len(sys.argv) < 3:
+        print("Usage: sbdl {local,qa,prod} {load_date} : Arguments are missing")
+        sys.exit(-1)
+
+    job_run_env = sys.argv[1].upper()
+    load_date = sys.argv[2]
+
+    spark = Utils.get_spark_session(job_run_env)
+    logger = Log4j(spark)
+
+    logger.info("Finished creating Spark Session")
